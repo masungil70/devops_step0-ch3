28db8c86259ca259f47acd524af5e8d6461d1a1c
masungil <masungil70@gmail.com>
2024-08-04 10:25:39 +0900

    수정

--- src/main/java/kr/ac/koreatech/devops_step0/HelloController.java
+++ src/main/java/kr/ac/koreatech/devops_step0/HelloController.java
@@ -2,7 +2,6 @@ package kr.ac.koreatech.devops_step0;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
